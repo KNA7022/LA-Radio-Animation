c63fc2b78873adba0db245a19113355a75bbd0f5
KNA7022 <89156599+KNA7022@users.noreply.github.com>
2023-02-14 15:12:01 +0800

    Add files via upload

---
+++ LARadioAnimation/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Cheng Johnson-chengkun'
+description 'Radio Animation with KeyMaping'
+version '1.0.0'
+
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
